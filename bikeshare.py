fdb0235cb2b43f458d0bc9e60113b112f4cbcc82
Noor98 <noorabohasira@gmail.com>
2023-09-14 23:27:21 +0300

    refactor: Change bikeshare.py file_change function get_filters name

--- bikeshare.py
+++ bikeshare.py
@@ -6,7 +6,7 @@ city_data = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
-def get_filters():
+def get_filterss():
     """
     Asks user to specify a city, month, and day to analyze.
 
@@ -283,7 +283,7 @@ def display_data(df):
     
 def main():
     while True:
-        city, month, day = get_filters()
+        city, month, day = get_filterss()
         df = load_data(city, month, day)
 
         time_stats(df)
